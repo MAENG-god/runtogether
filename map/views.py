d269efdea81788783876a238061771c0d78b65fd
Kim mingi <kmk2737@snu.ac.kr>
2022-08-04 23:51:46 +0900

    add map

--- map/views.py
+++ map/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
 def map(request):
-    return HttpResponse("hi~!")
+    template = loader.get_template("map/map.html")
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
