799900a5a751c65124ce3df59ecce66a0aeca1a4
Kim mingi <kmk2737@snu.ac.kr>
2022-08-11 18:32:38 +0900

    fuck

--- runtogether/settings.py
+++ runtogether/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^-20pdm00z@p9lemxf=$k51l@l(mv7_hnz9y_mm*mas!4tg=ei'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["django-env.eba-3xxmwnmv.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
